00060251432d064e4c606f4abc47f97536849f8b
Attinder Singh <attinderkahlon@gmail.com>
2022-08-06 11:43:01 +1200

    extra props added in StandardCard component

--- src/components/cards/StandardCard.tsx
+++ src/components/cards/StandardCard.tsx
@@ -1,10 +1,28 @@
 type Props = {
   name: string;
   image: string;
+  changePercentage: number;
+  currentPrice: number;
+  high24h: number;
+  low24h: number;
+  marketCap: number;
+  marketCapRank: number;
   onCardClick: () => void;
 };
 
-const StandardCard = ({ name, image, onCardClick }: Props) => {
+const StandardCard = ({
+  name,
+  image,
+  changePercentage,
+  currentPrice,
+  high24h,
+  low24h,
+  marketCap,
+  marketCapRank,
+  onCardClick,
+}: Props) => {
+  const percentChangeColor =
+    changePercentage > 0 ? "text-green-500" : "text-red-500";
   return (
     <div
       onClick={onCardClick}
@@ -14,30 +32,30 @@ const StandardCard = ({ name, image, onCardClick }: Props) => {
         <div className="h-12 w-12">
           <img src={image} alt={name} />
         </div>
-        <div className="grid">
+        <div className="grid text-sm">
           <h3>{name}</h3>
           <div className="flex space-x-2">
-            <span>$1001</span>
-            <span>3%</span>
+            <span>{"AUD " + currentPrice}</span>
+            <span className={percentChangeColor}>{changePercentage + "%"}</span>
           </div>
         </div>
       </div>
-      <div className="mt-3 grid [&>*>span]:text-gray-400 group-hover:[&>*>span]:text-white">
-        <div className="flex justify-between">
-          <span>Some option</span>
-          <p>12345</p>
+      <div className="mt-3 grid [&>*>span]:text-sm [&>*>span]:text-gray-400 group-hover:[&>*>span]:text-white [&>*]:flex [&>*]:justify-between">
+        <div>
+          <span>High (24h)</span>
+          <p>{high24h}</p>
         </div>
-        <div className="flex justify-between">
-          <span>Some option</span>
-          <p>12345</p>
+        <div>
+          <span>Low (24h)</span>
+          <p>{low24h}</p>
         </div>
-        <div className="flex justify-between">
-          <span>Some option</span>
-          <p>12345</p>
+        <div>
+          <span>Market Cap</span>
+          <p>{marketCap}</p>
         </div>
-        <div className="flex justify-between">
-          <span>Some option</span>
-          <p>12345</p>
+        <div>
+          <span>Market Cap Rank</span>
+          <p>{marketCapRank}</p>
         </div>
       </div>
     </div>
