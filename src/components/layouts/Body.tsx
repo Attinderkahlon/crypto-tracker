458d917314b02f6baa2ec42b62d02e99cccb0166
Attinder Singh <attinderkahlon@gmail.com>
2022-08-06 02:22:28 +1200

    Body component added

---
+++ src/components/layouts/Body.tsx
@@ -0,0 +1,12 @@
+import StandardCard from "../cards/StandardCard";
+
+const Body = () => {
+  return (
+    <div>
+      <h1 className="my-6">Explore blockchains</h1>
+      <StandardCard />
+    </div>
+  );
+};
+
+export default Body;
