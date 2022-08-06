8c5d153e3c4519fb18aed58ad3b754c570f2516b
Attinder Singh <attinderkahlon@gmail.com>
2022-08-07 10:38:28 +1200

    coin detail compnent added

--- src/pages/CoinDetail.tsx
+++ src/pages/CoinDetail.tsx
@@ -1,51 +1,65 @@
-import AreaSplineChart from "../components/charts/AreaSplineChart"
-import { useParams } from "react-router-dom"
-// import { Coin } from "../utils/types"
-import axios from "axios"
-import {
-  useState,
-  useEffect,
-  JSXElementConstructor,
-  ReactElement,
-  ReactFragment,
-  ReactPortal,
-} from "react"
-import Loader from "../components/Loader"
+import AreaSplineChart from "../components/charts/AreaSplineChart";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getChartDataByDaysAndId, getCoins } from "../utils/api";
+import { chartData, Coin } from "../utils/types";
+import { format } from "date-fns";
+import NotFound from "./NotFound";
 
 const CoinDetail = () => {
-  const params = useParams()
-  const [coin, setCoin] = useState<any>()
+  const { coinId } = useParams();
+  const [coin, setCoin] = useState<Coin>();
 
-  const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`
+  const [chartData, setChartData] = useState<chartData>();
 
   useEffect(() => {
-    axios
-      .get(url.toLowerCase())
-      .then((res) => {
-        setCoin(res.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }, [url])
+    console.log(coinId);
+    coinId &&
+      getChartDataByDaysAndId(coinId, 30).then((res) => {
+        setChartData(res.data);
+      });
+    getCoins().then((res) => {
+      setCoin(res.data?.find((coin: Coin) => coin.id === coinId));
+    });
+  }, [coinId]);
+
+  const labels = chartData?.prices.map((price) =>
+    format(new Date(price[0]), "dd MMMM")
+  );
+
+  const data =
+    chartData && chartData.prices.map((price) => Math.trunc(price[1]));
+
+  useEffect(() => {
+    console.log(coin);
+  }, [coin]);
 
   return (
-    <div className="h-full w-full bg-gray-900 ">
-      <h1 >Coin Detail</h1>
-      {!coin ? (
-          <Loader />
-        ) : (
-          <div className="mx-auto max-w-6xl px-4">
-                {coin.image && <img src={coin.image} alt="" />}
-                <p>{coin.name}</p>
-                {coin.symbol && <p>{coin.symbol.toUpperCase()}</p>}
-                {coin.market_data?.current_price && (
-                  <h1>${coin.market_data.current_price.aud.toLocaleString()}</h1>
-                )}
-           </div>)}
-           {/* <AreaSplineChart /> */}
-      </div>
- 
-  )
-}
-export default CoinDetail
+    <>
+      {chartData ? (
+        <div className="h-full w-full">
+          <h1>Coin Detail</h1>
+          {coin && (
+            <div className="grid">
+              <div className="flex h-20 w-full items-center space-x-4 border-2">
+                <img src={coin.image} alt={coin.name} className="h-16" />
+                <div className="grid h-min">
+                  <span className="text-sm text-gray-500">{coin.symbol}</span>
+                  <h1>{coin.name}</h1>
+                </div>
+                <div className="grid h-min">
+                  <span className="text-sm text-gray-500">Price (AUD)</span>
+                  <h1 className="font-semibold">${coin.current_price}</h1>
+                </div>
+              </div>
+            </div>
+          )}
+          {labels && data && <AreaSplineChart labels={labels} data={data} />}
+        </div>
+      ) : (
+        <NotFound />
+      )}
+    </>
+  );
+};
+export default CoinDetail;
