276f9a86cfa9b59af429a18332720945f33b65e9
Attinder singh <attinderkahlon@gmail.com>
2022-09-05 16:35:39 +1000

    update

--- src/pages/FuturePrices.tsx
+++ src/pages/FuturePrices.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
     const FuturePrices=  () =>{
         return (
           <>
-            <h1 className='text-center mt-5'>Sorry Chad, Still working on Time machine</h1>
+            <h1 className='text-center mt-5'>Sorry, Still working on Time machine</h1>
           <div className='flex flex-col items-center justify-center h-screen'>
          <img src={"/images/giphy.gif"} alt="Time" />
         <img src={"/images/giphy2.gif"} alt="Machine" />
